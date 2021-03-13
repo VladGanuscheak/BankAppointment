438256001b7d09044d5b603145dfeae974121105
Ganuşceac Vlad <vlad.ganusceac@faf.utm.md>
2021-03-13 21:03:37 +0200

    fix

---
+++ BankAppointmentScheduler.Administrative/BranchManagement/BranchManagement.cs
@@ -0,0 +1,56 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using BankAppointmentScheduler.Administrative.BranchManagement.Requests;
+using BankAppointmentScheduler.Common.Exceptions;
+using BankAppointmentScheduler.Domain;
+using BankAppointmentScheduler.Domain.BankEntities.Entities;
+
+namespace BankAppointmentScheduler.Administrative.BranchManagement
+{
+    public class BranchManagement : IBranchManagement
+    {
+        private readonly IBankAppointmentContext _context;
+
+        public BranchManagement(IBankAppointmentContext context)
+        {
+            _context = context;
+        }
+
+        public async Task CreateBranch(CreateBranchViewModel request, CancellationToken cancellationToken = default)
+        {
+            var entity = request.Cast();
+
+            await _context.Branches.AddAsync(entity, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task UpdateBranch(UpdateBranchViewModel request, CancellationToken cancellationToken = default)
+        {
+            var entity = await _context.Branches.FindAsync(request.BranchId, request.BankId);
+            if (entity == null)
+                throw new NotFoundException(nameof(Branch), new {request.BranchId, request.BankId});
+
+            var mappedEntity = request.Map(entity);
+
+            _context.Branches.Update(mappedEntity);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+
+        public async Task ConfigureBankSchedule(ConfigureBankScheduleViewModel request, CancellationToken cancellationToken = default)
+        {
+            var entity = request.OldWeekDay.HasValue
+                ? await _context.Schedules.FindAsync(request.BranchId, request.OldWeekDay)
+                : null;
+
+            var mappedEntity = entity == null 
+                ? request.Cast() 
+                : request.Map(entity);
+
+            await _context.Schedules.AddAsync(mappedEntity, cancellationToken);
+
+            await _context.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
